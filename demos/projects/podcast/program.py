8056ab2a5dda2c12cdb4708463af87c16a69b395
David Buck <david.x.buck@icloud.com>
2018-11-27 13:22:13 +0000

    Initial commits

--- demos/projects/podcast/program.py
+++ demos/projects/podcast/program.py
@@ -2,14 +2,18 @@ import service
 
 
 def main():
-    print("Welcome")
-    print()
+    print_welcome()
 
     service.download_info()
 
     for show_id in range(100, 141):
         info = service.get_episode(show_id)
         print("{}. {}".format(info.show_id, info.title))
+
+
+def print_welcome():
+    print("Welcome")
+    print()
 
 
 if __name__ == '__main__':
